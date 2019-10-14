fc37f6e9cb4ab25a376b3bfd64ffaa60c47c259e
krnithishkumar <nithishkumarkr@gmail.com>
2019-10-14 22:45:25 +0530

    Fixed Pgm7.py On input like '}{}' since it provides index error to previous code

--- pgm7.py
+++ pgm7.py
@@ -3,15 +3,20 @@ s = input()
 for i in s:
   if i in ['{','(','[']:
     stack.append(i)
-  elif(i == '}' and stack[-1]=='{'):
-    stack.pop()
-  elif(i==')' and stack[-1]=='('):
-    stack.pop()
-  elif(i==']' and stack[-1]=='['):
-    stack.pop()
+  elif(len(stack)!=0):
+    if(i == '}' and stack[-1]=='{'):
+      stack.pop()
+    elif(i==')' and stack[-1]=='('):
+      stack.pop()
+    elif(i==']' and stack[-1]=='['):
+      stack.pop()
+    else:
+      print("Not Balanced")
+      exit(0)
   else:
     print("Not Balanced")
     exit(0)
+  #print(stack)
 if(stack == []):
   print("Balanced")
 else:
